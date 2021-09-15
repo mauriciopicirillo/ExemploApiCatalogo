ed1fe98d74471bb4be8709091085f4d6d8fd2079
mauriciopicirillo <mauriciopicirillo@gmail.com>
2021-09-15 20:39:39 -0300

    uma parte ja feita

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Repositories/SqlConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace ExemploApiCatalogoJogos.Repositories
+{
+    internal class SqlConnection
+    {
+        private string v;
+
+        public SqlConnection(string v)
+        {
+            this.v = v;
+        }
+    }
+}
